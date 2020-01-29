079e4e6e18663acdd80aab9880d1b49119cf80f5
ilya <ilya@2cit.ru>
2020-01-29 15:43:14 +0300

    update

--- cf-dns.tf
+++ cf-dns.tf
@@ -1,9 +1,35 @@
-resource "cloudflare_record" "vpn-servers" {
+resource "cloudflare_record" "vpn-server-host" {
 
   count = var.servers_count
 
   zone_id = var.cloudflare_zone_id
-  name    = "${var.aws_region}.${var.dns_domain}"
+  name    = "${element(aws_instance.vpn.*.id, count.index)}.${var.aws_region}"
+  value   = element(aws_instance.vpn.*.public_ip, count.index)
+  type    = "A"
+  ttl     = 600
+
+  depends_on = [aws_instance.vpn]
+}
+
+resource "cloudflare_record" "vpn-domain-region-name" {
+
+  count = var.servers_count
+
+  zone_id = var.cloudflare_zone_id
+  name    = "${var.aws_region}.${var.dns_vpn_subdomain}.${var.dns_global_domain}"
+  value   = element(aws_instance.vpn.*.public_ip, count.index)
+  type    = "A"
+  ttl     = 600
+
+  depends_on = [aws_instance.vpn]
+}
+
+resource "cloudflare_record" "vpn-domain-default" {
+
+  count = var.servers_count
+
+  zone_id = var.cloudflare_zone_id
+  name    = "${var.dns_vpn_subdomain}.${var.dns_global_domain}"
   value   = element(aws_instance.vpn.*.public_ip, count.index)
   type    = "A"
   ttl     = 600
--- variables.tf
+++ variables.tf
@@ -3,8 +3,9 @@ variable "ssh_key_name" {}
 variable "servers_count" {
     default = "1"
 }
-variable "dns_domain" {}
+variable "dns_vpn_subdomain" {}
 variable "cloudflare_zone_id" {}
 variable "vpn_ami_id" {}
 variable "vpn_instance_type" {}
 variable "ansible_project" {}
+variable "dns_global_domain" {}
